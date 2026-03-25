f1041042f6b25dd04d541f0c9349764935d40cce
SamuelCandinho <samuel_e_candinho@estudante.sesisenai.org.br>
2026-03-25 12:55:27 -0300

    commit init

---
+++ scripts.js
@@ -0,0 +1,30 @@
+function soma() {
+
+}
+function subtracao() {
+
+}
+function multiplicacao() {
+    
+}
+function divisao() {
+    
+}
+function enter() {
+    
+}
+function limpar () {
+
+}
+function apagar () {
+
+}
+function ponto () {
+
+}
+function porcentagem () {
+
+}
+function numeros () {
+
+}
