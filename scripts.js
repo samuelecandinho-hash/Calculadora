a41857f6f20fb8bf8a857882740ea52c7782ab9d
SamuelCandinho <samuel_e_candinho@estudante.sesisenai.org.br>
2026-03-27 11:48:47 -0300

    a

--- scripts.js
+++ scripts.js
@@ -14,23 +14,6 @@ function divisao() {
     resul = Valor1 / Valor2
 }
 
-function limpar () {
-
-}
-function apagar () {
-
-}
-function ponto () {
-
-}
-function porcentagem () {
-    if (Contagem == 1) {
-        Valor1 = Valor1 / 100;
-    }
-}
-function numeros () {
-
-}
 function enter() {
 
 }
@@ -38,7 +21,7 @@ function display () {
     
 }
 function textdisplay(){
-let ass = 0;
+
 document.getElementById("myText").innerHTML = number1;
 }
 function num1(){
@@ -46,26 +29,38 @@ function num1(){
     textdisplay();
 }
 function num2(){
-    
+     number1 += 2;
+    textdisplay();
 }
 function num3(){
-    
+     number1 += 3;
+    textdisplay();
 }
 function num4(){
-    
+     number1 += 4;
+    textdisplay();
 }
 function num5(){
-    
+     number1 += 5;
+    textdisplay();
 }
 function num6(){
-    
+     number1 += 6;
+    textdisplay();
 }
 function num7(){
-    
+     number1 += 7;
+    textdisplay();
 }
 function num8(){
-    
+     number1 += 8;
+    textdisplay();
 }
 function num9(){
-    
+      number1 += 9;
+    textdisplay();
+}
+function num0(){
+      number1 += 0;
+    textdisplay();
 }
